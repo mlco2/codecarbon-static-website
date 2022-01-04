0bb5ce063020c710b8d78977ea3e284f531dc382
william <alown666@gmail.com>
2022-01-04 13:16:37 +0100

    reowrk of the navbar

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,4 +1,18 @@
 import Carrousel from './carrousel.js';
 
 new Carrousel(document.querySelector('.carrousel'));
-// console.log(test);
+
+let $navBar = document.querySelector('header');
+let $isSticky = document.querySelector('#isSticky');
+
+let observer = new IntersectionObserver(
+    function (entries) {
+        console.log(entries);
+        if (entries[0].intersectionRatio === 0) $navBar.dataset.isSticky = true;
+        else if (entries[0].intersectionRatio === 1)
+            $navBar.dataset.isSticky = false;
+    },
+    { threshold: [0, 1] }
+);
+
+observer.observe($isSticky);
